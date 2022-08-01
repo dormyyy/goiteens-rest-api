4430949de6af4da660522378ca9c041daa8f8339
dormyyy <sania.crafter@gmail.com>
2022-08-01 13:51:22 +0300

    new routes

--- routes/confirmator.py
+++ routes/confirmator.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 import ast
 import json
 from app import app, session
@@ -18,14 +18,14 @@ def get_current_confirmations():
     else:
         half = 1
     for i in [i.date_start for i in weeks]:
-        if 0 < (date - i).days <= 7:
+        if 0 <= (date - i).days <= 7:
             week_id = session.query(Weeks).filter_by(date_start=i).first().id
     slots = session.query(Slots).filter_by(date=date).all()
     slots_id = [i.id for i in slots]
     appointments = []
     for i in slots_id:
         appointments.append(session.query(Appointment).filter_by(slot_id=i).first())
-    day = slots[0].week_day
+    day = datetime.now().weekday()
     result.update({"week_id": week_id, "day": day, "half": half, "appointments": []})
     for i in appointments:
         if half == 1:
@@ -96,10 +96,34 @@ def get_confirmations(week_id: int, day: int, half: int):
 @app.route('/set_confirmation/<int:slot_id>/<int:status>/<string:message>/', methods=['PUT'])
 def set_confirmation(slot_id: int, status: int, message: str):
     appointment = session.query(Appointment).filter_by(slot_id=slot_id).first()
-    appointment.comments = message
-    session.commit()
-    slot = session.query(Slots).filter_by(id=slot_id).first()
-    slot.status = status
-    session.commit()
-    return jsonify(message="Successfully"), 200
+    if appointment:
+        appointment.comments = message
+        slot = session.query(Slots).filter_by(id=slot_id).first()
+        slot.status_id = status
+        session.commit()
+        return jsonify(message="Successfully"), 200
+    else:
+        return jsonify(message="Appointment not found"), 404
 
+
+@app.route('/set_cancel_confirmation/<int:slot_id>/<int:cancel_type>/<string:message>/', methods=['PUT'])
+def set_cancel_confirmations(slot_id: int, cancel_type: int, message: str):
+    appointment = session.query(Appointment).filter_by(slot_id=slot_id).first()
+    if appointment:
+        appointment.cancel_type = cancel_type
+        appointment.comments = message
+        session.commit()
+        return jsonify(message="Відмінено"), 200
+    else:
+        return jsonify(message="Appointment not found"), 404
+
+
+@app.route('/set_postpone_confirmation/<int:slot_id>/<int:appointment_id>/', methods=['PUT'])
+def set_postpone_confirmations(slot_id: int, appointment_id: int):
+    appointment = session.query(Appointment).filter_by(id=appointment_id).first()
+    if appointment:
+        appointment.slot_id = slot_id
+        session.commit()
+        return jsonify(message="Перенесено"), 200
+    else:
+        return jsonify(message="Appointment not found"), 404
