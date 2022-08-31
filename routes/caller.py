f56f5e641c35574405ea98639bdee370cea16663
dormyyy <sania.crafter@gmail.com>
2022-08-31 22:33:49 +0300

    caller major fix

--- routes/caller.py
+++ routes/caller.py
@@ -39,16 +39,16 @@ def get_caller_current_week():
     for i in result:
         if i == []:
             result.remove(i)
-    try:
-        for i in result:
-            for j in i:
-                for _, p in j.items():
-                    if type(p) is list:
-                        for m in p:
-                            manager_id = m['manager_id']
+    for i in result:
+        for j in i:
+            for _, p in j.items():
+                if type(p) is list:
+                    for m in p:
+                        manager_id = m['manager_id']
+                        if session.query(Manager).filter_by(id=manager_id).first() is not []:
                             m['name'] = session.query(Manager).filter_by(id=manager_id).first().name
-    except:
-        pass
+                        else:
+                            m['name'] = 'not found'
     try:
         data_to_json.to_json(result)
     except:
@@ -91,7 +91,10 @@ def get_caller_week(week_id: int):
                     if type(p) is list:
                         for m in p:
                             manager_id = m['manager_id']
-                            m['name'] = session.query(Manager).filter_by(id=manager_id).first().name
+                            if session.query(Manager).filter_by(id=manager_id).first() is not []:
+                                m['name'] = session.query(Manager).filter_by(id=manager_id).first().name
+                            else:
+                                m['name'] = 'not found'
         try:
             data_to_json.to_json(result)
         except:
