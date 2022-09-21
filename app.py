e4874986c58b444d65e630792466f662d2d07165
dormyyy <sania.crafter@gmail.com>
2022-09-21 16:52:53 +0300

    minor fix

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_marshmallow import Marshmallow
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
 ma = Marshmallow(app)
