e694861a9457751aa88ed6445dbd71c61d0bb82e
Mikhail <newlevelcenter@gmail.com>
2022-10-13 11:16:03 +0300

    sorting_confirmations

--- routes/confirmator.py
+++ routes/confirmator.py
@@ -6,7 +6,17 @@ from models import *
 import backup
 from utils.convert_str_to_datetime import get_current_date, get_current_hour
 from utils import data_to_json
+from operator import itemgetter
 
+
+def appointment_hour_sort(n):
+    try:
+        x = int(n["hour"])
+        
+    except TypeError:
+        x=21
+    print(x)
+    return x
 
 @app.route('/current_confirmation', methods=['GET'])
 def get_current_confirmations():
@@ -87,11 +97,17 @@ def get_current_confirmations():
 def get_confirmations(week_id: int, day: int, half: int):
     result = {}
     date = session.query(Weeks).filter_by(id=week_id).first().date_start + timedelta(days=day)
+    # print(date)
     slots = session.query(Slots).filter_by(date=date, status_id=3).all()
+    # print(slots)
     slots_id = [i.id for i in slots]
+    # print(slots_id)
     appointments = []
+    # print(Appointment)
     for i in slots_id:
+        print(i)
         appointments.append(session.query(Appointment).filter_by(slot_id=i, cancel_type=0).first())
+    print(appointments)
     result.update({"week_id": week_id, "day": day, "half": half, "date": datetime.now().date(), "appointments": []})
     for i in appointments:
         if i is not None:
@@ -145,6 +161,13 @@ def get_confirmations(week_id: int, day: int, half: int):
         backup.backup()
     except:
         print('', end='')
+    # result = sorted(result,key = appointment_hour_sort)
+    result["appointments"] = sorted(result["appointments"],key =lambda n: n['hour'] )
+    # for i in result["appointments"]:
+    #   print(i['hour'])
+    # res = appointment_hour_sort(result)
+    # print(res)
+    # print(result)
     return jsonify(message="Successfully", data=result), 200
 
 
