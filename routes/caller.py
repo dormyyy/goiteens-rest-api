c16a00dfa410d9f43e7db7373dd03eb7a9081c7d
Mikhail <newlevelcenter@gmail.com>
2022-11-13 14:26:31 +0200

    managers_list_hour8

--- routes/caller.py
+++ routes/caller.py
@@ -155,9 +155,11 @@ def get_available_managers_list(week_id: int, week_day: int):
             print(slot.id)
             print(slot.manager_id)
             manager = session.query(Manager).filter_by(id = slot.manager_id).first()
-            print(manager)
-            print(manager.id)
-            hour_result.append({'manager_id': manager.id, 'name':manager.name} )
+            # print(manager)
+            print(f"{manager.id} - {manager.name}")
+            el= {'manager_id': manager.id, 'name':manager.name}
+            print(el)
+            hour_result.append(el )
 
     # hour_result = [{'manager_id': 1, 'name':'name'} ]
     # hour_result = [{'manager_id': i.id, 'name': i.name} for i in managers]
