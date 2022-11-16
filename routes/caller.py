1538034db112557c1827da8e83ae4c18c8437d40
Mikhail <newlevelcenter@gmail.com>
2022-11-16 04:04:40 +0200

    manager_slot_list

--- routes/caller.py
+++ routes/caller.py
@@ -58,6 +58,58 @@ def get_caller_current_week():
         print('', end='')
     return jsonify(current_week_id=current_week_id, current_week_date_start=current_week.date_start, slots=result), 200
 
+@app.route('/caller_current_week_manager/<int:manager_id>', methods=['GET'])
+def get_caller_current_week_manager(manager_id: int):
+    weeks = session.query(Weeks).all()
+    current_date = get_current_date()
+    for i in [i.date_start for i in weeks]:
+        if 0 <= (current_date - i).days <= 7:
+            current_week_id = session.query(Weeks).filter_by(date_start=i).first().id
+    current_week = session.query(Weeks).filter_by(id=current_week_id).first()
+    template = [{"time": i, "amount": 0} for i in range(8,23)]
+    current_week_days = []
+    result = []
+    for i in range(0,7):
+        current_week_days.append(current_week.date_start + timedelta(days=i))
+    for date in current_week_days:
+        current_day_slots = []
+        slots = session.query(Slots).filter_by(date=date, status_id=1,manager_id=manager_id).all()
+        if len(slots) == 0:
+            result.extend([template])
+        else:
+            for i in range(8, 23):
+                slot = session.query(Slots).filter_by(date=date, time=i, status_id=1,manager_id=manager_id).all()
+                if i in [j for j in current_day_slots]:
+                    continue
+                if len(slot) == 0:
+                    current_day_slots.append({"time": i, "amount": 0})
+                else:
+                    current_day_slots.append({"time": i, "amount": len(slot), "slots": slots_schema.dump(slot)})
+        result.extend([current_day_slots])
+    for i in result:
+        if i == []:
+            result.remove(i)
+    for i in result:
+        for j in i:
+            for _, p in j.items():
+                if type(p) is list:
+                    for m in p:
+                        try:
+                            manager_id = m['manager_id']
+                            if session.query(Manager).filter_by(id=manager_id).first():
+                                m['name'] = session.query(Manager).filter_by(id=manager_id).first().name
+                            else:
+                                m['name'] = 'not found'
+                        except:
+                            print()
+    try:
+        backup.backup()
+    except:
+        print('', end='')
+    return jsonify(current_week_id=current_week_id, current_week_date_start=current_week.date_start, slots=result), 200
+
+
+
 # Отримати аналітику - скільки часів.
 @app.route('/get_caller_week/<int:week_id>', methods=['GET'])
 def get_caller_week(week_id: int):
