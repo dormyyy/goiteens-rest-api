94515b40186245f6d78c8b9ecbbaaf31ab44d53a
Mikhail <newlevelcenter@gmail.com>
2022-12-11 13:21:48 +0200

    av_manager

--- routes/managers.py
+++ routes/managers.py
@@ -65,6 +65,83 @@ def get_avaliable_managers():
         print('', end='')
     return jsonify(data=result)
 
+@app.route('/av_managers/<int:week_id>/<int:day>/<int:half>/', methods=['GET'])
+def get_av_managers():
+    result = {}
+    date = session.query(Weeks).filter_by(id=week_id).first().date_start + timedelta(days=day)
+    # print(date)
+    slots = session.query(Slots).filter_by(date=date, status_id=1).all()
+    # print(slots)
+    slots_id = [i.id for i in slots]
+    # print(slots_id)
+    appointments = []
+    # print(Appointment)
+    for i in slots_id:
+        print(i)
+        appointments.append(session.query(Appointment).filter_by(slot_id=i, cancel_type=0).first())
+    print(appointments)
+    result.update({"week_id": week_id, "day": day, "half": half, "date": datetime.now().date(), "appointments": []})
+    for i in appointments:
+        if i is not None:
+            if half == 1:
+                course = session.query(Course).filter_by(id=i.course_id).first()
+                if course:
+                    course_name = course.name
+                else:
+                    course_name = 'No course'
+                if session.query(Manager).filter_by(
+                            id=session.query(Slots).filter_by(id=i.slot_id).first().manager_id).first():
+                    manager_name = session.query(Manager).filter_by(
+                            id=session.query(Slots).filter_by(id=i.slot_id).first().manager_id).first().name
+                else:
+                    manager_name = 'not found'
+                if session.query(Slots).filter_by(id=i.slot_id).first().time < 14:
+                    result["appointments"].append({
+                        "appointment_id": i.id,
+                        "hour": session.query(Slots).filter_by(id=i.slot_id).first().time,
+                        "course": course_name,
+                        "manager_name": manager_name,
+                        "crm_link": i.zoho_link,
+                        "phone": i.phone,
+                        "slot_id": i.slot_id,
+                        "status": session.query(Slots).filter_by(id=i.slot_id).first().status_id
+
+                    })
+            else:
+                course = session.query(Course).filter_by(id=i.course_id).first()
+                if course:
+                    course_name = course.name
+                else:
+                    course_name = 'No course'
+                if session.query(Slots).filter_by(id=i.slot_id).first().time >= 14:
+                    try:
+                        m_name = session.query(Manager).filter_by(
+                            id=session.query(Slots).filter_by(id=i.slot_id).first().manager_id).first().name,
+                    except:
+                        m_name = 'Manager'
+                    result["appointments"].append({
+                        "appointment_id": i.id,
+                        "hour": session.query(Slots).filter_by(id=i.slot_id).first().time,
+                        "course": course_name,
+                        "manager_name": m_name,
+                        "crm_link": i.zoho_link,
+                        "phone": i.phone,
+                        "slot_id": i.slot_id,
+                        "status": session.query(Slots).filter_by(id=i.slot_id).first().status_id
+                    })
+    try:
+        backup.backup()
+    except:
+        print('', end='')
+    # result = sorted(result,key = appointment_hour_sort)
+    result["appointments"] = sorted(result["appointments"],key =lambda n: n['hour'] )
+    # for i in result["appointments"]:
+    #   print(i['hour'])
+    # res = appointment_hour_sort(result)
+    # print(res)
+    # print(result)
+    return jsonify(message="Successfully", data=result), 200
+
 
 # /manager/update/<int:manager_id>
 @app.route('/update_manager/<int:manager_id>', methods=['PUT'])
