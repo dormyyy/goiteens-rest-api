2aae8e04ab9890fc168d5d7edc4f916fc6a649ad
Yaroslav <manypresents34@gmail.com>
2023-04-19 01:17:06 +0300

    new week's endpoint

--- routes/work_weeks.py
+++ routes/work_weeks.py
@@ -1,5 +1,7 @@
+from typing import Tuple
+
 from app import app, session
-from flask import request, jsonify
+from flask import request, jsonify, Response
 from models import *
 from schemas import *
 from utils.convert_str_to_datetime import to_datetime, get_current_date
@@ -66,3 +68,17 @@ def get_active_week_id():
             current_week_id = session.query(Weeks).filter_by(date_start=i).first().id
             backup.backup()
             return jsonify(week_id=current_week_id)
+
+
+@app.route('/get_week_id/<string:date>', methods=['GET'])
+def get_week_id(date: str) -> tuple[Response, int]:
+    try:
+        weeks = session.query(Weeks).all()
+        current_date = to_datetime(date).date()
+        for i in [i.date_start for i in weeks]:
+            if 0 <= (current_date - i).days <= 7:
+                week = session.query(Weeks).filter_by(date_start=i).first()
+        result = week_schema.dump(week)
+        return jsonify(result), 200
+    except Exception as e:
+        return jsonify(error=str(e)), 400
